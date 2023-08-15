303647baf20c29acb5872a01ab9d1c32518249ba
kingslayer845 <ahmed051042@gmail.com>
2023-08-15 10:40:48 +0300

    init client

---
+++ client/src/main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root/Root";
+import ErrorPage from "./routes/root/ErrorPage";
+const router = createBrowserRouter([
+  { path: "/", element: <Root />, errorElement: <ErrorPage /> },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
